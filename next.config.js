af1f529ba83e6a5f441dbfd73ff53d7daba713c6
Natnael Biset <sjemae1029@gmail.com>
2024-06-10 18:51:59 +0300

    added destination on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,10 @@
 module.exports = {
     async rewrites() {
-        return [
-          {
-	source: "/api/(.*)",
-        headers: [
-	{ key: "Access-Control-Allow-Credentials",
-	  value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3000",
-          },
-          // Allows for specific methods accepted
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          // Allows for specific headers accepted (These are a few standard ones)
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-	  
-        ],
-      },
-        ]
-      },
-  };
+       return [
+         {
+            source: '/api/:path*',
+            destination: 'http://localhost:3000/:path*'
+         }
+      ]
+  }
+}

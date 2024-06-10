4de4335dbba9ebf208f3bb9f8c49515e15a586c0
Natnael Biset <sjemae1029@gmail.com>
2024-06-10 20:13:51 +0300

    sample ip for post api

--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -8,7 +8,7 @@ export default function SignUp(){
   const handleSubmit = async (event) => {
     event.preventDefault();
     
-    const response = await fetch('http://127.0.0.1:3000/api/cookie', {
+    const response = await fetch('https://loqui-web.onrender.com/api/cookie', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',

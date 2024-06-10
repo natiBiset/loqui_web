5e85ae856f7dad5faeb67ffb5cdaf8a35ed53885
Natnael Biset <sjemae1029@gmail.com>
2024-06-10 19:39:57 +0300

    cleared config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-async headers() {
-    return [
-      {
-	source: "/api/(.*)",
-        headers: [
-	{ key: "Access-Control-Allow-Credentials",
-	  value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3000",
-          },
-          // Allows for specific methods accepted
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          // Allows for specific headers accepted (These are a few standard ones)
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-	  
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = { };
 
 export default nextConfig;

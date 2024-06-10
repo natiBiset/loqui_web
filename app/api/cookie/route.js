612741d7a6b4c3508d829a4058506a15d7102269
Natnael Biset <sjemae1029@gmail.com>
2024-06-10 16:34:19 +0300

    added /api/cookie/route.js

---
+++ app/api/cookie/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+export async function POST(request) {
+  const { username } = await request.json();
+  console.log(username)
+  const userID = Math.random().toString(16).slice(2);
+
+  const response = NextResponse.json({ success: true });
+  response.cookies.set('signedIn', 'true', { path: '/' });
+  response.cookies.set('username', username, { path: '/' });
+  response.cookies.set('id', userID, { path: '/' });
+
+  return response;
+}

b8394be4914f4f91c7a3765f521dd99af528bdd5
Natnael Biset <sjemae1029@gmail.com>
2024-06-10 16:36:06 +0300

    cors issue for api routing

--- app/api/cookie/route.js
+++ app/api/cookie/route.js
@@ -5,6 +5,9 @@ export async function POST(request) {
   const userID = Math.random().toString(16).slice(2);
 
   const response = NextResponse.json({ success: true });
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'POST');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
   response.cookies.set('signedIn', 'true', { path: '/' });
   response.cookies.set('username', username, { path: '/' });
   response.cookies.set('id', userID, { path: '/' });

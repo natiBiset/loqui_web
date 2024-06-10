cc98eddffbacbcc73c7ab0276822b35ed1da7266
Natnael Biset <sjemae1029@gmail.com>
2024-06-10 18:49:26 +0300

    added next.config.js file

---
+++ next.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    async rewrites() {
+        return [
+          {
+	source: "/api/(.*)",
+        headers: [
+	{ key: "Access-Control-Allow-Credentials",
+	  value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3000",
+          },
+          // Allows for specific methods accepted
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          // Allows for specific headers accepted (These are a few standard ones)
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+	  
+        ],
+      },
+        ]
+      },
+  };
